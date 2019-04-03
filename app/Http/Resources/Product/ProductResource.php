c4665d7479d676f8e309f7acb898c6f8cd659929
sukantahui <huisukanta77@gmail.com>
2019-04-03 08:47:18 +0530

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock==0 ? 'No Stock' : $this->stock,
             'discount'=>$this->discount,
+            'totalPrice'=>(1-($this->discount/100))*$this->price,
+            'rating'=>$this->reviews->count()>0? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'href'=>[
+                'reviews' => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
